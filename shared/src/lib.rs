38cb556377e5249f45470499d726219df6d5c62d
Indrajit Vaidyanathan <vindrajit1996@gmail.com>
2025-07-03 02:25:54 -0700

    Added button core component

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -9,5 +9,7 @@ pub use error::*;
 pub fn add_templates(tera: &mut Tera) {
     tera.add_template_files(vec![
         ("./shared/templates/macros/forms.html", Some("macros/forms.html")),
+        ("./shared/templates/macros/components.html", Some("macros/components.html")),
+        ("./shared/templates/macros/icons.html", Some("macros/icons.html")),
     ]).expect("Failed to load shared macros");
 }
